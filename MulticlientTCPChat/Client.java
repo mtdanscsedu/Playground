bca3ef531161ef38c872ae52d54235c23357217b
mtdanscsedu <mtdanscsedu@gmail.com>
2015-11-18 23:53:26 -0600

    Update Client.java

--- MulticlientTCPChat/Client.java
+++ MulticlientTCPChat/Client.java
@@ -1,1 +1,45 @@
 
+package com.ashif.net;
+
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+/**
+ *
+ * @author Ashif
+ */
+public class Client {
+    
+    
+    public static void initialize_client(Socket sock, String User_Name){
+        try {
+            
+            DataOutputStream output= new DataOutputStream(sock.getOutputStream());
+            output.writeUTF(User_Name);
+            
+        } catch (Exception e) {
+            System.out.println("Exception Inside Client initialize_client "+e);
+        }
+      
+    }
+    
+    public static void main(String[] args) throws IOException {
+    
+        Scanner in= new Scanner(System.in);
+        System.out.print("Please give your user name : ");
+        String inFromUser=in.nextLine();
+        Socket sock=new Socket("localhost",3500);
+        
+        initialize_client(sock,inFromUser);
+        System.out.println("Client Window");
+        
+        
+        new Thread(new Sender(sock, inFromUser)).start();
+        new Thread(new Receiver(sock)).start();
+    }   
+    
+}
+
